1ac8c091c9a40f415a8e63fdf275c347c90bc4f8
Konke Radlow <koradlow@gmail.com>
2013-01-16 21:35:32 +0000

    Integrated sleep timer into main algorithm, Improved long term sync stability of main algorithm
    Signed-off-by: Konke Radlow <koradlow@gmail.com>

--- Projects/Examples/Applications/main_babble.c
+++ Projects/Examples/Applications/main_babble.c
@@ -44,6 +44,17 @@
 #include "stdio.h"
 #include "string.h"
 
+/* unique ID for this network node, also used to decide the order of the algorithm steps */
+#define UNIQUE_ID 0x00
+/* number of iterations of the (broadcast-listen) or (listen-broadcast) loop */
+#define BROADCAST_ITERATIONS 4
+/* radio active period during broadcast and listen step (in multiples of 10ms)*/
+#define RADIO_PERIOD 50
+/* sleep period between broadcasting cycles (in seconds) */
+#define SLEEP_PERIOD 10
+/* set the number of bitfields to keep in memory */
+#define BITFIELD_MEMORY 10
+
 /* the message type is used in the first byte of every message to define what kind
  * of data is carried by the message */
 typedef enum msg_type
@@ -53,15 +64,6 @@ typedef enum msg_type
 	msg_count = 0x04
 }msg_type;
 
-/* unique ID for this network node, also used to decide the order of the algorithm steps */
-#define UNIQUE_ID 0x01
-/* number of iterations of the (broadcast-listen) or (listen-broadcast) loop */
-#define BROADCAST_ITERATIONS 4	
-/* radio active period during broadcast and listen step (in multiples of 100ms)*/
-#define RADIO_PERIOD 10
-/* set the number of bitfields to keep in memory */
-#define BITFIELD_MEMORY 10
-
 /* to be able to use bit operations we have to use integers to represent the bitfield.
  * The devices are capable of tranmitting up to 10bytes in one transmission. As the first
  * byte of every message is used to define the message type, we can count up to 
@@ -70,9 +72,13 @@ typedef enum msg_type
 static uint32_t bitfieldA;
 static uint8_t buffer[MAX_APP_PAYLOAD];
 
-/* two global variables used for the period functionality */
+/* global variables used for the active period functionality */
 static volatile bool activePeriod;
 static volatile uint32_t activePeriodOfLimit;
+/* global variables used for the sleep timer configuration */
+static unsigned char st2 =0, st1=0, st0=0;
+static unsigned long countVal=0;
+static unsigned long offset = 0;
 
 /* reserve memory space in XDATA memory locations, that contains data in sleep modes 2 and 3 
  * and is used to store the last n bitfields */
@@ -84,26 +90,23 @@ __xdata static uint8_t bfIdx = 0;
 __interrupt void timer1IR(void);
 
 /* function prototypes */
-static void countingAlgorithm(void);
+static void countingAlgorithm(bool joinNetwork);
 static void broadcastBitfield(void);
 static void listenBitfield(void);
 static void broadcastSync(void);
-static void waitSync(void);
+static bool waitSync(void);
 static void storeBitfield(const uint32_t *bitfield);
 static void transmitBitfields(void);
 static void setActivePeriod(uint32_t timeoutX100ms);
-static void setupSleepTimer(void);
-static void goToSleep(uint8_t durationX1s);
-
-static unsigned char st2 =0, st1=0, st0=0;
-static unsigned long countVal=0;
+static void initSleepTimer(void);
+static void exitSleepTimer(void);
+static void sleepPm2(uint32_t seconds);
 
 void main (void)
 {
 	/* initialize the board hardware */
-	setupSleepTimer();
 	BSP_Init();
-	
+
 	/* Assign a unique address to the radio device, based on the the unique NW id.
 	 * The first three bytes can be selected arbitrarlily */
 	addr_t lAddr = {{0x71, 0x56, 0x34, UNIQUE_ID}};
@@ -113,30 +116,23 @@ void main (void)
 	 * in this scenario. but we don't care -- just use the default link token later.
 	 * we supply a callback pointer to handle the message returned by the peer */
 	SMPL_Init(0);
-	
-	/* test the sleep timer functionality */
-	BSP_TURN_OFF_LED1();
-	goToSleep(1);
-	BSP_TURN_ON_LED1();
-	NWK_DELAY(1000);
-	BSP_TURN_OFF_LED1();
-	goToSleep(1);
-	BSP_TURN_ON_LED1();
-	
+		
 	/* wait for a sync message or a button press to start the algorithm ... */
-	waitSync();
-	
-	/* start executing the algorithm */
-	countingAlgorithm();
+	countingAlgorithm(waitSync());
 }
 
-static void countingAlgorithm()
+/* realizes the counting algorithm for the low energy mesh-network architecture
+ * In: joinNetwork - if the node joins an existing network the first broadcast of
+ * 					the sync message will be skipped */
+static void countingAlgorithm(bool joinNetwork)
 {
 	while (1)
 	{ 
 		/* broadcast that a new algorithm iteration starts, to allow new nodes
-		 * to join in */
-		broadcastSync();
+		 * to join in. Nodes that join for the first time will skip this call */
+		if (!joinNetwork) 
+			broadcastSync();
+		joinNetwork = false;
 		
 		/* start with listening / broadcasting depending on the unique id */
 		if (UNIQUE_ID % 2 == 0)
@@ -163,10 +159,9 @@ static void countingAlgorithm()
 		/* store the bitfield in a list in memory */
 		storeBitfield(&bitfieldA);
 					  
-		/* spoof MCU sleeping... */
+		/* send SOC to sleep */
 		BSP_TURN_OFF_LED1();
-		setActivePeriod(100);
-		while (activePeriod);
+		sleepPm2(SLEEP_PERIOD);
 	}
 }
 
@@ -183,7 +178,8 @@ static void broadcastBitfield()
 	buffer[0] = msg_count;
 	memcpy(&buffer[1], &bitfieldA, sizeof(bitfieldA));
 	
-	/* broadcast the current bitfield one time */
+	/* broadcast the current bitfield one single time, then shutdown the radio
+	 * and wait for the transmit period to expire */
 	bool bcast_sent = false;
 	setActivePeriod(RADIO_PERIOD);
 	while (activePeriod)
@@ -192,11 +188,15 @@ static void broadcastBitfield()
 		{
 			SMPL_Send(SMPL_LINKID_USER_UUD, buffer, sizeof(buffer));
 			bcast_sent = true;
+			/* shut the radio down early as possible to save energy */
+			SMPL_Ioctl( IOCTL_OBJ_RADIO, IOCTL_ACT_RADIO_SLEEP, 0);
 		}
 	}
-	
-	/* shut the radio down */
-	SMPL_Ioctl( IOCTL_OBJ_RADIO, IOCTL_ACT_RADIO_SLEEP, 0);
+
+	/* fixed delay after active period for simulating shutting down the radio
+	 * to keep duration of listenBitfield() and broadcastBitfield() equal */
+	setActivePeriod(1);
+    while(activePeriod);
 }
 
 static void listenBitfield()
@@ -233,14 +233,29 @@ static void listenBitfield()
 		}
 	}
 	
-	/* shut the radio down */
-    SMPL_Ioctl( IOCTL_OBJ_RADIO, IOCTL_ACT_RADIO_SLEEP, 0);
+	/* fixed delay after active period for shutting down the radio to keep 
+	 * duration of listenBitfield() and broadcastBitfield() equal */
+	setActivePeriod(1);
+	bool radioShutDown = false;
+    while(activePeriod) 
+	{
+		/* shut the radio down */
+		if (!radioShutDown) 
+		{
+			SMPL_Ioctl( IOCTL_OBJ_RADIO, IOCTL_ACT_RADIO_SLEEP, 0);
+			radioShutDown = true;
+		}
+	}
 }
 
-static void waitSync()
+/* enables the receiver and waits for a sync message from the network or a button
+ * press. If a button press is detected, the node will start it's own network 
+ * Return: true - if sync message was received
+ * 		   false - if button press was detected */
+static bool waitSync()
 {
 	uint8_t len = 0;
-	
+	bool syncMessageReceived = false;
 	BSP_TURN_OFF_LED1();
 	
 	/* wake up radio. We need it to listen for broadcasts */
@@ -259,6 +274,7 @@ static void waitSync()
 			BSP_TURN_ON_LED1();
 			if (buffer[0] == msg_sync) {
 				/* sync message from network received -> start the algorithm */ 
+				syncMessageReceived = true;
 				exitLoop = true;
 			}
 		}
@@ -272,6 +288,8 @@ static void waitSync()
 	
 	/* shut the radio down */
 	SMPL_Ioctl( IOCTL_OBJ_RADIO, IOCTL_ACT_RADIO_SLEEP, 0);
+	
+	return syncMessageReceived;
 }
 
 /* broadcasts a message of the syncronization type */
@@ -335,10 +353,10 @@ void transmitBitfields(void)
 /* sets the board up to be able to use the sleep timer for waking up from power
  * mode 2
  * !!  MUST BE EXECUTED BEFORE ENTERING POWER MODES !! */
-void setupSleepTimer(void)
+void initSleepTimer(void)
 {
 	/* Switch to High Speed RC Oscillator */
-
+	
 	/* Power up the high speed RC Osc (SLEEP.OSC_PD = 0) */
 	SLEEP &= ~0x04;
 	/* Wait until this Osc is stable (SLEEP.HFRC_STABLE = 1) */
@@ -351,9 +369,32 @@ void setupSleepTimer(void)
 	SLEEP |= 0x04;
 }
 
+/* re-configures the board to the same configuration is was in before entering
+ * sleep mode (switch to christal oscillator) */
+void exitSleepTimer(void){
+	/* Power up HS XOSC (SLEEP.OSC_PD = 0) */
+	SLEEP &= ~0x04;
+	/* Wait until XOSC is stable (SLEEP.XOSC_STB = 1) */
+	while(!(SLEEP & 0x40));
+	/* Apply NOPs */
+	asm("NOP");
+	/* Switch system clock to HS XOSC (CLKCON.OSC = 0) */
+	CLKCON &= ~0x40;
+	/* Wait until clock has changed */
+	while(CLKCON & 0x40);
+	/* Power down HS RCOSC (SLEEP.OSC_PD=1) */
+	SLEEP|= 0x04; 
+}
+
 /* sends the device to sleep mode 2 for n seconds */
-static void goToSleep(uint8_t durationX1s)
+static void sleepPm2(uint32_t seconds)
 {
+	/* Setup 16Mhz clock as system clock - this is necessary to enter sleep mode */
+	initSleepTimer();
+	
+	/* Convert number of sleep seconds into sleep timer cycles */
+	offset = seconds * 32768;
+	
 	/* Set Sleep mode to PM2 */
 	SLEEP = (SLEEP & 0xFC) | 0x02;
 	
@@ -369,10 +410,14 @@ static void goToSleep(uint8_t durationX1s)
 	* will not be entered */
 	if(SLEEP & 0x03)
 	{
-		/* Clear interrupt flags */
+		/* variables to store current register values */
+		unsigned char ircon_prev = 0, ien_prev = 0;
+		/* Store current interrupt register & clear interrupt flags */
+		ircon_prev = IRCON;
 		IRCON = 0x00;
 		
-		/* Enable Sleep Timer interrupt */
+		/* Store interrupt mask register & enable Sleep Timer interrupt */
+		ien_prev = IEN0;
 		IEN0 |= 0xA0;
 		
 		/* read the current count values for sleep timer, and use them to calculate
@@ -387,9 +432,10 @@ static void goToSleep(uint8_t durationX1s)
 		
 		/* add an offset to the count value, defining the sleep duration 
 		* formula: offset = t_sleep / 32kHz */
-		countVal = countVal + 0x28000;
+		countVal = countVal + offset;
+		if(countVal>0xFFFFFF){countVal = countVal - 0xFFFFFF;}
 		
-		/* set the new compare values for sleep timer */
+		 /* set the new compare values for sleep timer */
 		ST2 = (countVal>>16) & 0xFF;
 		ST1 = (countVal>>8) & 0xFF;
 		ST0 = countVal & 0xFF;
@@ -404,12 +450,14 @@ static void goToSleep(uint8_t durationX1s)
 		
 		/* Apply a NOP as first instruction when exiting sleep mode */
 		asm("NOP");
+
+		/* Reestablish 32MHz clock as system clock */
+		exitSleepTimer();
 		
-		/* Clear interrupt flags */
-		IRCON = 0x00;
-		
-		/* Disable Sleep Timer interrupt */
-		IEN0 |= 0x00;
+		/* restore register values to status before entering sleep mode */
+		SLEEP &= 0xFC; 	// set cpu to pm0
+		IRCON = ircon_prev;
+		IEN0 = ien_prev;
 	}
 }
 
@@ -432,15 +480,15 @@ void setActivePeriod(uint32_t period)
 	/* set the clock divede prescaler to the lowest possible value */
 	T1CTL = 0x0C; //set the clk divide to 128 -> increment every 4us
 	
-	/* generate an interrupt every 100ms
-	 * --> use module mode, with a compare value of 4us * 25000 = 100ms*/
+	/* generate an interrupt every 10ms
+	 * --> use module mode, with a compare value of 4us * 2500 = 10ms*/
 	union modulo
 	{ 
 		uint16_t val_16; 
 		uint8_t val_8[2]; 
 	};
 	union modulo mod;
-	mod.val_16 = 25000;
+	mod.val_16 = 2500;
 	
 	T1CC0L = mod.val_8[0];
 	T1CC0H = mod.val_8[1];
